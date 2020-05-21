f95dc7989c9516f2de5e9f17a9eafef8e92ef4d9
connor <vbrookesy@hotmail.co.uk>
2020-05-21 16:57:32 +0100

    Added an operatus into the digits ...

--- src/App.js
+++ src/App.js
@@ -13,8 +13,16 @@ class App extends Component {
     animals: [
       {name: "Pete", species: "Duck"},
       {name: "Dave", species: "Pigeon"}
-    ]
+    ],
+    digits: [1, 2, 3, 4]
   }
+  
+  addNumber = (number) => {
+    this.setState({
+      digits: [... this.state.digits, number]
+    })
+  }
+
   render() {
     const eachPerson = this.state.persons.map((student, index) => {
       return <Aboutme name = {student.name} age = {student.age} key = {index}/>
@@ -22,11 +30,17 @@ class App extends Component {
     const eachAnimal = this.state.animals.map((pet, index) => {
       return <Pets name = {pet.name} species = {pet.species} key = {index}/>
     })
+    const eachNumber = this.state.digits.map((singleNumber, index)=> {
+      return <li key={index}>{singleNumber}</li>
+    })
     return(
       <div>
 
          {eachPerson}
          {eachAnimal}
+         <ul>{eachNumber}</ul>
+        {/* <input type = "number" onChange={this.giveMeMyNumber} value={this.state.currentNumber} /> */}
+        <button onClick={()=> this.addNumber(10)}></button>
       </div>
     )
   }
